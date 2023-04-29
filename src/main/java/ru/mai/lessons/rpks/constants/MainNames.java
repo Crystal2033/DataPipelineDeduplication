7500bc907db9f3514d4e80da742cadb1af354ab9
Kulikov Pavel <kulikov-pavlik@bk.ru>
2023-04-29 15:08:44 +0300

    Connect redis to main arch and tested some examples.

---
+++ src/main/java/ru/mai/lessons/rpks/constants/MainNames.java
@@ -0,0 +1,16 @@
+package ru.mai.lessons.rpks.constants;
+
+public class MainNames {
+
+    private MainNames() {
+
+    }
+
+    public static final String CONF_PATH = "application.conf";
+    public static final String TOPIC_NAME_PATH = "topic.name";
+    public static final String KAFKA_NAME = "kafka";
+
+    public static final String NAME_STRING_VALUE = "name";
+    public static final String AGE_STRING_VALUE = "age";
+    public static final String SEX_STRING_VALUE = "sex";
+}
