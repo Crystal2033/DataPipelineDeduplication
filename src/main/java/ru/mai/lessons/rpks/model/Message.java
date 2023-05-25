1ff4d511f22df74b101ef8202e00462473ea0855
Anton <anton.komissarov.02@mail.ru>
2023-05-25 12:33:41 +0300

    Prepare configs and copy db and kafka from lab_1

--- src/main/java/ru/mai/lessons/rpks/model/Message.java
+++ src/main/java/ru/mai/lessons/rpks/model/Message.java
@@ -1,12 +1,13 @@
 package ru.mai.lessons.rpks.model;
 
-import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
-@Builder
+@RequiredArgsConstructor
 public class Message {
+    @NonNull
     private String value; // сообщение из Kafka в формате JSON
-
-    private boolean deduplicationState; // true - удовлетворены условиях всех правил (Rule), false - хотя бы одно условие не прошло проверку.
+    private boolean deduplicationState = false; // true - удовлетворены условиях всех правил (Rule), false - хотя бы одно условие не прошло проверку.
 }
